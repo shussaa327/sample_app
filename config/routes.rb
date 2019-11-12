6e72bffae3b6044d45e255887e18f960f0f86e12
shunsuke sasaki <shunsukesasaki@shunsukenoMacBook-Pro.local>
2019-11-12 19:30:29 +0900

    [Finish] 3-5

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'todolists/new'
   get "top" => "root#top"
+  post "todolists" => "todolists#create"
 end
