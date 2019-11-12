cb8ef2a7f92cef7ab8ed85b8bd4f8a9a7d2c0b0b
shunsuke sasaki <shunsukesasaki@shunsukenoMacBook-Pro.local>
2019-11-12 23:29:10 +0900

    [Finish] 3-6

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get 'todolists/new'
   get "top" => "root#top"
   post "todolists" => "todolists#create"
+  get "todolists" => "todolists#index"
+  get "todolists/:id" => "todolists#show", as: "todolist"
 end
